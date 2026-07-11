018bdf3a83bf061ce1a50dec8dd7a24c96898e5c
natbonn <natbon@hotmail.gr>
2026-07-11 15:25:57 +0300

    Add SQL scripts to insert initial data for teachers and courses

---
+++ src/main/resources/db/insert_into_courses.sql
@@ -0,0 +1,9 @@
+INSERT INTO courses (id, title, teacher_id) VALUES
+    (1, 'Java',   1),
+    (2, 'Cobol',  1),
+    (3, 'C#',     2),
+    (4, 'Python', 3),
+    (5, 'SQL',    4),
+    (6, 'React',  4);
+
+ALTER TABLE courses AUTO_INCREMENT = 7;
