d1db3229a34e7686fd32ea07a807999d639dde73
natbonn <natbon@hotmail.gr>
2026-07-11 20:33:52 +0300

    Refactor Main to implement JPQL queries for selecting teachers and courses

--- src/main/java/gr/aueb/cf/Main.java
+++ src/main/java/gr/aueb/cf/Main.java
@@ -2,10 +2,9 @@ package gr.aueb.cf;
 
 import gr.aueb.cf.model.Course;
 import gr.aueb.cf.model.Teacher;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.Persistence;
+import jakarta.persistence.*;
+
+import java.util.List;
 
 public class Main {
 
@@ -19,11 +18,40 @@ public class Main {
         try {
             tx.begin();
 
-            Course java = em.find(Course.class, 1L);
-            Teacher alice = em.find(Teacher.class, 1L);
+            // JPQL
 
-            alice.removeCourse(java);
-            em.remove(java);
+            // Select all teachers
+//            String query = "SELECT t FROM Teacher t";
+//            TypedQuery<Teacher> typedQuery = em.createQuery(query, Teacher.class);
+//            List<Teacher> teachers = typedQuery.getResultList();
+//            teachers.forEach(System.out::println);
+
+            // Select all courses
+//            String query2 = "SELECT c FROM Course c";
+//            List<Course> courses = em.createQuery(query2, Course.class).getResultList();
+//            courses.forEach(System.out::println);
+
+            // Select courses of Καπέτης
+//            String query3 = "SELECT c FROM Course c WHERE c.teacher.lastname = 'Καπέτης'";
+//            List<Course> kapetisCourses = em.createQuery(query3, Course.class).getResultList();
+//            kapetisCourses.forEach(System.out::println);
+
+            // Select courses of Καπέτης SQL Injection free
+            String query4 = "SELECT c FROM Course c WHERE c.teacher.lastname = :lastname";
+            List<Course> courses4 = em
+                    .createQuery(query4, Course.class)
+                    .setParameter("lastname", "Γιαννούτσου")
+                    .getResultList();
+            courses4.forEach(System.out::println);
+
+            tx.commit();
+
+//            REMOVE ==============================
+//            Course java = em.find(Course.class, 1L);
+//            Teacher alice = em.find(Teacher.class, 1L);
+//
+//            alice.removeCourse(java);
+//            em.remove(java);
 
 //            Teacher alice = em.find(Teacher.class, 1L);
 //            Course java = new Course("Java");
@@ -36,7 +64,7 @@ public class Main {
 //            Teacher alice = new Teacher("Alice", "Smith");
 //            em.persist(alice);                // insert
 
-            tx.commit();
+
         } catch (RuntimeException e) {
             if (tx.isActive()) tx.rollback();
             throw e;
